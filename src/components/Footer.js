c879ddf17ec714685949d35a537ee8f6eabb2533
CalvinDoesCS <calvinntran.cs@gmail.com>
2024-06-17 22:17:33 -0400

    Added Assets + Layout

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import Nav from './Nav'
+
+const Footer = () => {
+  return (
+    <footer>
+        <Header/>
+        <Nav/>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import image from "../icons_assets/Logo.svg"
+const Header = () => {
+  return (
+    <header>
+        <img src={image} alt="Little Lemon Logo"/>
+    </header>
+  )
+}
+
+export default Header
