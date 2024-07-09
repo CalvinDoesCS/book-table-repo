29c804af06db21c8aff2d58fa1318afb4d54dcab
CalvinDoesCS <calvinntran.cs@gmail.com>
2024-07-09 16:39:10 -0400

    add link component for homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,10 @@
-import Footer from "../components/Footer.js"
 import "../homepage.css"
 import foodImage from '../icons_assets/restauranfood.jpg'
-import Button from '../components/Button.js'
 import Specials from '../components/Specials.js'
 import CustomersSay from '../components/CustomersSay.js'
 import Chicago from '../components/Chicago.js'
-import Header from '../components/Header.js'
-import Nav from '../components/Nav.js'
+import { Link } from "react-router-dom"
+import Button from "../components/Button.js"
 const Homepage = () => {
   return (
     <>
@@ -16,7 +14,7 @@ const Homepage = () => {
             <h1> Little Lemon</h1>
             <h2> Chicago </h2>
             <p> We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist</p>
-            <Button>Reserve A Table</Button>
+            <Button><Link to="booking">Reserve A Table</Link></Button>
           </div>
           <div className='header-img'>
             <img src={foodImage} alt="man holding food"/>
